4748d51b1ae84857798ba292d8b69cbdac562a0d
Elyor <gaybnazarov96@gmail.com>
2023-06-29 17:24:34 +0500

    improved alert message if user is offline

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -6,20 +6,51 @@ import getMovies from '../../Api';
 import './CardList.css';
 
 const ERROR_MESSAGE = 'Connection Timeout, please check your internet connection';
+const IS_OFFLINE_MESSAGE = 'You are offline';
 
 export default class CardList extends Component {
-  state = { movies: [], loading: true, error: false };
+  state = { movies: [], loading: true, error: false, isOnline: true };
   keyIterator = 1;
 
-  componentDidMount() {
+  createMovies() {
     getMovies('return').then(
       (res) => {
-        this.setState({ movies: res.results, loading: false });
+        this.setState({ movies: res.results, loading: false, error: false });
       },
       (error) => {
         this.setState({ error: true, loading: false, errorText: error.toString() });
       }
     );
+  }
+
+  handleNetworkChange = () => {
+    this.setState((prevState) => ({ isOnline: !prevState.isOnline }));
+  };
+
+  componentDidMount() {
+    this.createMovies();
+    window.addEventListener('online', this.handleNetworkChange);
+    window.addEventListener('offline', this.handleNetworkChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.handleNetworkChange);
+    window.removeEventListener('offline', this.handleNetworkChange);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const currentState = this.state;
+    if (currentState.isOnline !== prevState.isOnline) {
+      if (navigator.onLine) {
+        this.createMovies();
+      } else {
+        this.setState({
+          error: true,
+          loading: false,
+          errorText: IS_OFFLINE_MESSAGE,
+        });
+      }
+    }
   }
 
   createMoviesCards(movies) {
@@ -29,7 +60,7 @@ export default class CardList extends Component {
           {movies.map((item) => {
             return (
               <Col key={this.keyIterator++} span={12}>
-                <Card searchResult={item} baseImgUrl={'https://image.tmdb.org/t/p/w500'} />
+                <Card searchResult={item} baseImgUrl={'https://image.tmdb.org/t/p/original'} />
               </Col>
             );
           })}
@@ -44,7 +75,7 @@ export default class CardList extends Component {
       <Alert className="errorStatus" message={ERROR_MESSAGE} description={errorText} type="error" showIcon />
     ) : null;
     const loadingStatus = loading ? <Spin className="loadingStatus" size="large"></Spin> : null;
-    const content = !(loading && error) ? this.createMoviesCards(movies) : null;
+    const content = !(loading || error) ? this.createMoviesCards(movies) : null;
     return (
       <>
         {loadingStatus}
